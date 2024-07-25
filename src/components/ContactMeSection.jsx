b2df0808f9504731bf75236c7fbff3d5664a3df9
Mohammed Adil <mokolokiii@gmail.com>
2024-07-25 23:03:37 +0300

    approved

--- src/components/ContactMeSection.jsx
+++ src/components/ContactMeSection.jsx
@@ -35,7 +35,7 @@ const LandingSection = () => {
       firstName: Yup.string().required("Required"),
       email: Yup.string().email("Invalid email address").required("Required"),
       comment: Yup.string()
-        .min(25, "Must be at least 25 characters")
+        .min(25, "Must be at least 25 characters") 
         .required("Required"),
     }),
   });
